6d5e249d4c758676803af7ca6d70a9423b63c58f
Siliciumo2 <147613900+siliciumo2@users.noreply.github.com>
2024-12-23 11:10:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        Process[] notepads = Process.GetProcessesByName("notepad");
+
+        foreach (Process notepad in notepads)
+        {
+            notepad.Kill();
+            notepad.Kill();
+            notepad.Kill();
+            Console.WriteLine("блокнот закрыт");
+        }
+    }
+}
